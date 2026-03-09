54fcf93608a932168d5a2f7388fb5cd9e277abcd
Anuj Rai <anujxilling253745@gmail.com>
2026-03-09 08:26:01 +0545

    new file update and workin in the project

---
+++ project3.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Student_info:
+    def __init__(self,name,age,Class):
+        self.name=name
+        self.age=age
+        self.Class=Class
+students={
+    "user1":{
+        "name":"anuj",
+        "age":17,
+        "class":18
+    }
+}
+print(students)
+n=int(input("enter the number of student:"))
+for i in range(n):
+    name=input(f"enter the name for std {i+1}:")
+    age=input(f"enter the age of std {name}:")
+    Class=input(f"enter the Class of std {name}:")
+    s=Student_info(name,age,Class)
+    
+print(students)
