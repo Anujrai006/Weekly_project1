11a3d74b94224e068777884e282d2407bd5bea83
Anuj Rai <anujxilling253745@gmail.com>
2026-03-15 05:21:16 +0545

    students data stored

--- side_test_pr.py
+++ side_test_pr.py
@@ -1,8 +1,12 @@
-name="s h y a m"
-new=name.split(" ")
-print(new)
-i=0
-while i<=len(new):
-    new_name=new[i]
-    new_name+=new_name
-print(new_name)
+# name="s h y a m"
+# new=name.split(" ")
+# print(new)
+# i=0
+# while i<=len(new):
+#     new_name=new[i]
+#     new_name+=new_name
+# print(new_name)
+# l=[1,2,3,4,5]
+# print(len(l))
+for i in range(1,2):
+    print(i)
