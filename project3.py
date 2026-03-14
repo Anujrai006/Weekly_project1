ad81f929b63267d73a7acb0c84525f259b75bd23
Anuj Rai <anujxilling253745@gmail.com>
2026-03-15 05:19:55 +0545

    new changes made in the functions

--- project3.py
+++ project3.py
@@ -42,26 +42,21 @@ def load_students():
         pass
 
     return students
-students = load_students()
-start = len(students) + 1
-
-try:
-    n = int(input("enter the number of student: "))
-except ValueError:
-    n = 0
-
-for i in range(start, start + n):
-    name = input(f"enter the name for std {i}: ")
-    age = input(f"enter the age of std {name}: ")
-    Class = input(f"enter the Class of std {name}: ")
-    s = Student_info(name, age, Class)
-    students[f"user{i}"] = {
-        "name": s.name,
-        "age": s.age,
-        "class": s.Class,
+students=load_students()    
+start=len(students)+1
+n=int(input("enter the number of student:"))
+for i in range(start,start+n):
+    name=input(f"enter the name for std {i}:")
+    age=input(f"enter the age of std {name}:")
+    Class=input(f"enter the Class of std {name}:")
+    s=Student_info(name,age,Class)
+    students[f"user{i}"]={
+        "name":s.name,
+        "age":s.age,
+        "class":s.Class,
     }
 def save_students(students):
-    with open("student.txt","a") as f:
+    with open("student.txt","w") as f:
         for key,value in students.items():
             f.write(f"{key},{value['name']},{value['age']},{value['class']}\n")
 save_students(students)
